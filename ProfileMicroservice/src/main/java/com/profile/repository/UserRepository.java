60fb9df57675f7963b2fdbfc659f26d9a666afd4
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2022-05-19 13:24:16 +0200

    feat: add sql

--- ProfileMicroservice/src/main/java/com/profile/repository/UserRepository.java
+++ ProfileMicroservice/src/main/java/com/profile/repository/UserRepository.java
@@ -1,16 +1,15 @@
 package com.profile.repository;
 
 import com.profile.model.User;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
     User findByUsername(String username);
-    
+
     @Query(value = "SELECT * FROM USERS WHERE LOWER(FIRST_NAME) LIKE LOWER(CONCAT('%', ?1, '%')) AND LOWER(LAST_NAME) LIKE LOWER(CONCAT('%', ?2, '%'))", nativeQuery = true)
     List<User> findAllByFirstNameAndLastName(String firstName, String lastName);
 }
--- ProfileMicroservice/src/main/resources/data.sql
+++ ProfileMicroservice/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO users (ID, BIOGRAPHY, DATE_OF_BIRTH, EMAIL, FIRST_NAME, GENDER, LAST_NAME, PHONE_NUMBER, USERNAME)
 VALUES ('d12602fd-b7af-4da1-b1ca-bad8166d1fb3', 'aaa', '05-05-2005', 'a@a.com', 'aca', 'MALE', 'aca', '+32242342', 'peraperic');
+INSERT INTO users (ID, BIOGRAPHY, DATE_OF_BIRTH, EMAIL, FIRST_NAME, GENDER, LAST_NAME, PHONE_NUMBER, USERNAME)
+VALUES ('d12602fd-b7af-4da1-b1ca-bad8166d1fb2', 'aaa', '05/05/2005', 'a@a.com', 'Aca', 'MALE', 'Aca', '+32242342', 'username1');
