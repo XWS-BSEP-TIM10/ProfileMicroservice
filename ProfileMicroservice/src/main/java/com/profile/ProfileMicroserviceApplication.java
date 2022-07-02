6b5a5a2c3cfafc540e9d0f051c815d25df5252dc
Anica Đukić <73143643+AnicaDjukic@users.noreply.github.com>
2022-07-02 17:18:19 +0200

    feat: made two channels

--- ProfileMicroservice/src/main/java/com/profile/ProfileMicroserviceApplication.java
+++ ProfileMicroservice/src/main/java/com/profile/ProfileMicroserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 @SpringBootApplication
 public class ProfileMicroserviceApplication {
@@ -19,10 +20,19 @@ public class ProfileMicroserviceApplication {
 			});
 
 			// subscribes to nats.demo.service channel
-			dispatcher.subscribe("nats.demo.service", msg -> {
+			/*dispatcher.subscribe("nats.demo.service", msg -> {
 				System.out.println("Received : " + new String(msg.getData()));
 				nats.publish(msg.getReplyTo(), "Hello Publisher from Profile Microservice!".getBytes());
+			});*/
+			// subscribes to nats.demo.service channel
+			AtomicReference<String> message = null;
+			dispatcher.subscribe("nats.demo.service", msg -> {
+				System.out.println("Received : " + new String(msg.getData()));
+				nats.publish("nats.demo.reply", "Message received, Profile".getBytes());
 			});
+
+
+
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
